4db66ccdeb79cb9b76a2e88600c750cfb433e89c
Natalia Sánchez <sanchnat@umich.edu>
2020-12-03 14:12:13 -0500

    Added the binomial coefficient improved algorithm

---
+++ binomialCoefficient.py
@@ -0,0 +1,20 @@
+# Note: this implementation is the Dynamic Programming solution to the binomial coefficient
+# from geeksforgeeks.com. The link is:
+# https://www.geeksforgeeks.org/binomial-coefficient-dp-9/
+
+def binomialCoeff(n , k):
+
+    # Declaring an empty array
+    C = [0 for i in xrange(k+1)]
+    C[0] = 1
+
+    for i in range(1,n+1):
+
+        # Compute next row of pascal triangle using
+        # the previous row
+        j = min(i ,k) 
+        while (j>0):
+            C[j] = C[j] + C[j-1]
+            j -= 1
+
+    return C[k]
